f462db6209351d89947ea15b236ab56cfd2a5206
Rahmi Kılıç <koray.kilic@metu.edu.tr>
2023-05-10 16:14:48 +0300

    redux fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import  BloodRequest  from './components/BloodRequest';
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
 import  SelectProduct  from "./components/SelectProduct";
+import { Whole } from "./components/Whole";
 
 
 export const URL = process.env.REACT_APP_SERVER_URL;
@@ -17,7 +18,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/bloodrequests' element={<BloodRequests />} />
           <Route path='/bloodrequest' element={<BloodRequest/>} />
-          <Route path='/bloodrequest/:id' element={<BloodRequest/>} />
+          <Route path='/bloodrequest/:id' element={<Whole/>} />
           <Route path='/selectproduct' element={<SelectProduct/>} />
         </Routes>
       </BrowserRouter>    
