701bdbb13a32db8173004693875c08d998be56a2
huseyin.cambasi <huseyincambasi@gmail.com>
2023-05-26 00:14:43 +0300

    register and login pages created

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BloodRequests } from './components/BloodRequests';
-import  BloodRequest  from './components/BloodRequest';
-import { Home } from './components/Home';
-import { Navbar } from './components/Navbar';
-import  SelectProduct  from "./components/SelectProduct";
-import { Whole } from "./components/Whole";
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { Home } from "./components/Home";
+import SignUp from "./scenes/signUpPage/";
+import SignIn from "./scenes/signInPage";
 
 export const URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/bloodrequests' element={<BloodRequests />} />
-          <Route path='/bloodrequest' element={<BloodRequest/>} />
-          <Route path='/bloodrequest/:id' element={<Whole/>} />
-          <Route path='/selectproduct' element={<SelectProduct/>} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>    
     </div>
   );
 }
 
 export default App;
+
+//<Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />}/>
+//<Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />}/>
--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./index.css";
 import App from './App';
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
-import { Whole } from './components/Whole';
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-const initialState={input: 
-  {
-        site:<Whole/>
-  },
-}
-
-function reducer(state=initialState,action){
-  switch(action.type){
-    case 'CHANGETYPE':
-      return {...state,site:action.newsite};
-      default:
-        return state;
-  }
-}
-
-const store=configureStore({reducer:reducer});
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
