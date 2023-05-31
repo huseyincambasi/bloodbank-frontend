d28f55953c408329c9df72931e5304dea7f873d6
Burhan Bahçecioğlu <burhan.bahcecioglu@inventanalytics.com>
2023-05-31 20:52:55 +0300

    add validation

--- src/components/DonateDialog.jsx
+++ src/components/DonateDialog.jsx
@@ -1,12 +1,23 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import { useSelector } from "react-redux";
-import { Button, Box, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, useTheme } from '@mui/material'
+import {
+    Button,
+    Box,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField,
+    useTheme,
+    Stepper, Step, StepLabel, Typography, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio
+} from '@mui/material'
 import axios from "axios";
 import { URL } from "App";
 
 const DonateDialog = (props) => {
     const theme = useTheme();
     const [dialogOpen, setDialogOpen] = useState(false);
+    const [activeStep, setActiveStep] = useState(0);
     const [dialogInputs, setDialogInputs] = useState({
         _id: "",
         firstName: "",
@@ -16,8 +27,13 @@ const DonateDialog = (props) => {
         email: "",
     });
 
-    const isAuth = Boolean(useSelector((state) => state.access_token));
+    const access_token = useSelector((state) => state.access_token);
+    const isAuth = Boolean(access_token);
     const user = useSelector((state) => state.user);
+
+    const [questions, setQuestions] = useState([]);
+    const [answers, setAnswers] = useState([]);
+
     useEffect(() => {
         if (dialogOpen && isAuth) {
             setDialogInputs((prevState) =>  ({
@@ -41,6 +57,29 @@ const DonateDialog = (props) => {
         }
     }, [props.selectedRow]);
 
+    useEffect(() => {
+        async function fetchQuestions() {
+            await axios.get(
+                `${URL}/api/blood_request/${props.selectedRow}/questions/`,
+                {headers: {Authorization: 'Bearer ' + access_token}}
+            ).then(
+                (response)=> {
+                    const data = response.data;
+                    setQuestions(data);
+
+                    let answer_array = [];
+                    for (let i = 0; i < data.length; i++) {
+                        answer_array[i] = ({["answer_" + i]: ""});
+                    }
+                    setAnswers(answer_array);
+                }
+            )
+        };
+        if (dialogOpen) {
+            fetchQuestions();
+        }
+    }, [dialogOpen]);
+
     const closeDialog = () => {
         setDialogOpen(false);
         props.selectedRowChanged(null);
@@ -52,6 +91,35 @@ const DonateDialog = (props) => {
             phoneNumber: "",
             email: "",    
         });
+        setActiveStep(0);
+        setAnswers([]);
+    };
+
+    const handleNext = () => {
+        if (activeStep === 1) {
+            sendEmail();
+        }
+        else {
+            axios.post(
+                `${URL}/api/blood_request/${props.selectedRow}/validate`, {answers},
+                {headers: {Authorization: 'Bearer ' + access_token}}
+            ).then(
+                (response)=> {
+                    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+                }
+            ).catch(
+                (error) => {
+                    if (error === 409) {
+                        closeDialog();
+                    }
+                    closeDialog();
+                }
+            )
+        }
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
 
     const handleChange = (e) => {
@@ -61,8 +129,18 @@ const DonateDialog = (props) => {
         }))
     };
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+    const handleAnswer = (questionIndex, answer) => {
+
+        const updated_answers = [
+            ...answers.slice(0, questionIndex),
+            {["answer_" + questionIndex]: answer.target.value},
+            ...answers.slice(questionIndex + 1)
+        ];
+        setAnswers(updated_answers);
+
+    };
+
+    const sendEmail = () => {
         axios.post(`${URL}/api/blood_requests/${dialogInputs._id}/donate`, dialogInputs)
         closeDialog();
     }
@@ -72,22 +150,113 @@ const DonateDialog = (props) => {
             <Dialog open={dialogOpen} onClose={closeDialog}>
                 <DialogTitle>Donate Blood</DialogTitle>
                 <DialogContent>
-                    <DialogContentText>
-                        To donate blood to this patient, please enter your information below. We
-                        will forward your information to the person requesting blood via e-mail.
-                    </DialogContentText>
-                    <TextField disabled={isAuth} required margin="dense" name="firstName" value={dialogInputs.firstName} onChange={handleChange} label="First Name" variant="standard" fullWidth sx={{mb: 3}} />
-                    <TextField disabled={isAuth} required margin="dense" name="lastName" value={dialogInputs.lastName} onChange={handleChange} label="Last Name" variant="standard" fullWidth sx={{mb: 3}}/>
-                    <TextField disabled={isAuth} required margin="dense" name="address" value={dialogInputs.address} onChange={handleChange} label="Address" variant="standard" fullWidth sx={{mb: 3}}/>
-                    <TextField disabled={isAuth} required margin="dense" name="phoneNumber" value={dialogInputs.phoneNumber} onChange={handleChange} label="Phone Number" variant="standard" fullWidth sx={{mb: 3}}/>
-                    <TextField disabled={isAuth} required margin="dense" name="email" value={dialogInputs.email} onChange={handleChange} label="Email" type="email" variant="standard" fullWidth sx={{mb: 3}}/>                
+                    <Stepper activeStep={activeStep}>
+                        <Step>
+                            <StepLabel>Questionnaire</StepLabel>
+                        </Step>
+                        <Step>
+                            <StepLabel>Input Form</StepLabel>
+                        </Step>
+                    </Stepper>
+                    <div>
+                        {activeStep === 0 ? (
+                            <div>
+                                <Typography variant="h6">Pick Yes or No</Typography>
+                                {questions.map((question, index) => (
+                                    <div key={index}>
+                                        <FormControl>
+                                            <FormLabel
+                                                id="validation_questions">{`Question ${index + 1}: ${question["question_" + index]}`}</FormLabel>
+                                            <
+                                                RadioGroup
+                                                name="controlled-radio-buttons-group"
+                                                value={answers["answer_" + index]}
+                                                onChange={(event) => handleAnswer(index, event)}
+                                            >
+                                                <FormControlLabel value="yes" control={<Radio/>} label="Yes"/>
+                                                <FormControlLabel value="no" control={<Radio/>} label="No"/>
+                                            </RadioGroup>
+                                        </FormControl>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <div>
+                                <Typography variant="h6">Input Form</Typography>
+                                <TextField
+                                    disabled={isAuth}
+                                    required
+                                    margin="dense"
+                                    name="firstName"
+                                    value={dialogInputs.firstName}
+                                    onChange={handleChange}
+                                    label="First Name"
+                                    variant="standard"
+                                    fullWidth
+                                    sx={{ mb: 3 }}
+                                />
+                                <TextField
+                                    disabled={isAuth}
+                                    required
+                                    margin="dense"
+                                    name="lastName"
+                                    value={dialogInputs.lastName}
+                                    onChange={handleChange}
+                                    label="Last Name"
+                                    variant="standard"
+                                    fullWidth
+                                    sx={{ mb: 3 }}
+                                />
+                                <TextField
+                                    disabled={isAuth}
+                                    required
+                                    margin="dense"
+                                    name="address"
+                                    value={dialogInputs.address}
+                                    onChange={handleChange}
+                                    label="Address"
+                                    variant="standard"
+                                    fullWidth
+                                    sx={{ mb: 3 }}
+                                />
+                                <TextField
+                                    disabled={isAuth}
+                                    required
+                                    margin="dense"
+                                    name="phoneNumber"
+                                    value={dialogInputs.phoneNumber}
+                                    onChange={handleChange}
+                                    label="Phone Number"
+                                    variant="standard"
+                                    fullWidth
+                                    sx={{ mb: 3 }}
+                                />
+                                <TextField
+                                    disabled={isAuth}
+                                    required
+                                    margin="dense"
+                                    name="email"
+                                    value={dialogInputs.email}
+                                    onChange={handleChange}
+                                    label="Email"
+                                    type="email"
+                                    variant="standard"
+                                    fullWidth
+                                    sx={{ mb: 3 }}
+                                />
+                            </div>
+                        )}
+                    </div>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={closeDialog} sx={{m: "2rem 0", p: "1rem", backgroundColor: theme.palette.primary.main, color: theme.palette.background.alt, "&:hover": { color: theme.palette.primary.main }}}>
                         Cancel
-                    </Button>                  
-                    <Button type="submit" onClick={sendEmail} sx={{m: "2rem 0", p: "1rem", backgroundColor: theme.palette.primary.main, color: theme.palette.background.alt, "&:hover": { color: theme.palette.primary.main }}}>
-                        Donate
+                    </Button>
+                    <Button disabled={activeStep === 0} onClick={handleBack} sx={{m: "2rem 0", p: "1rem", backgroundColor: theme.palette.primary.main, color: theme.palette.background.alt, "&:hover": { color: theme.palette.primary.main }}}>
+                        Back
+                    </Button>
+                    <Button onClick={handleNext} sx={{m: "2rem 0", p: "1rem", backgroundColor: theme.palette.primary.main, color: theme.palette.background.alt, "&:hover": { color: theme.palette.primary.main }}}>
+                        {activeStep === 1 ? 'Finish' : 'Next'}
                     </Button>
                 </DialogActions>
             </Dialog>
